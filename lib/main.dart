b7e39de73c6943ed18c21219032bfd37e21137a4
Arjun khousi <info@sakshamdigitaltechnology.com>
2026-04-04 16:48:43 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,14 @@ import 'package:bigreminder/screens/auth/signup_screen.dart';
 import 'package:bigreminder/services/auth/auth_gate.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main()  async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(ProviderScope(child: const MyApp()));
 }
 
