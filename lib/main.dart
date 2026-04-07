8075f129fb45ed2548df667dfbbed815024585af
Flutter-Project <nooralam91152@gmail.com>
2026-04-07 12:06:19 +0530

    Noor 07/04/2026

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,31 @@
 import 'package:bigreminder/providers/theme_provider.dart';
-import 'package:bigreminder/screens/auth/login_screen.dart';
-import 'package:bigreminder/screens/auth/signup_screen.dart';
 import 'package:bigreminder/services/auth/auth_gate.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 👇 Firebase initialize
+  await Firebase.initializeApp();
+
   runApp(ProviderScope(child: const MyApp()));
 }
 
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ref.watch(themeProvider),
-      home: AuthGate(),
+      home: const AuthGate(),
     );
   }
 }
-// noor
+
+
+
+// Noor
